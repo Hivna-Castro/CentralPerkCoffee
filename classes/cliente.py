d48fa15e06cc8d6f0549df75d9f17a5069e45af6
Hivna-Castro <hivna12@gmail.com>
2025-07-07 23:39:10 -0300

    feat: implementa a classe Cliente e função de processamento de pedidos

---
+++ classes/cliente.py
@@ -0,0 +1,4 @@
+class Cliente:
+    def __init__(self, nome: str, frequente: bool):
+        self.nome = nome
+        self.frequente = frequente
--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 from fabrica_produto import FabricaProduto
+from classes.desconto import DescontoClienteFrequente, DescontoPromocaoDia, DescontoCupomPersonalizado
+from classes.pedido import Pedido
+from classes.cliente import Cliente
+from classes.notificador import NotificadorCliente, NotificadorCozinha, LoggerSistema
 from exceptions import ProdutoCadastradoError
 
-def main():
+def processar_pedido(cliente, produtos):
+    if cliente.frequente:
+        desconto = DescontoClienteFrequente()
+    else:
+        desconto = DescontoPromocaoDia()
+    pedido = Pedido(desconto)
     fabrica = FabricaProduto()
+    try:
+        for produto in produtos:
+            pedido.adicionar_produto(fabrica.criar_produto(produto))
+    except ProdutoCadastradoError as e:
+        print(e)
+    pedido.registrar_notificador(NotificadorCliente())
+    pedido.registrar_notificador(NotificadorCozinha())
+    pedido.registrar_notificador(LoggerSistema())
+    print(f"--- Pedido do cliente {cliente.nome} ---")
+    pedido.finalizar_pedido()
 
-    produto = fabrica.criar_produto("cafe_latte")
-    print(f"Produto: {produto.nome}, R${produto.preco_base}, {produto.preparo}")
+# Testando com clientes diferentes
+cliente1 = Cliente("Ross", frequente=True)
+cliente2 = Cliente("Monica", frequente=False)
 
-if __name__ == "__main__":
-    main()
+processar_pedido(cliente1, ["cafe", "brownie"])
+processar_pedido(cliente2, ["cafe", "brownie"])
